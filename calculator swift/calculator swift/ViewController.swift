f9917f63185856bec47f853165ecb77760ef3502
Ainur <sibrinat616@gmail.com>
2023-11-02 15:33:52 +0300

    another functions done, dot bug fixed

--- calculator swift/calculator swift/ViewController.swift
+++ calculator swift/calculator swift/ViewController.swift
@@ -1,11 +1,13 @@
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
     @IBOutlet weak var displayResultLabel: UILabel!
     var stillTyping = false
     var firstOperand: Double = 0
     var secondOperand: Double = 0
+    var dotIsPlaced = false
     var operationSign: String = ""
     
     var currentInput: Double {
@@ -14,7 +16,13 @@ class ViewController: UIViewController {
         }
         
         set {
-            displayResultLabel.text = "\(newValue)"
+            let value = "\(newValue)"
+            let valueArray = value.components(separatedBy: String("."))
+            if valueArray[1] == "0" {
+                displayResultLabel.text = "\(valueArray[0])"
+            } else {
+                displayResultLabel.text = "\(newValue)"
+            }
             stillTyping = false
         }
     }
@@ -23,7 +31,8 @@ class ViewController: UIViewController {
         operationSign = sender.currentTitle!
         firstOperand = currentInput
         stillTyping = false
-    }
+        dotIsPlaced = false
+     }
     
     @IBAction func numberPressed(_ sender: UIButton!) {
         let number = sender.currentTitle!
@@ -44,6 +53,9 @@ class ViewController: UIViewController {
         if stillTyping {
             secondOperand = currentInput
         }
+        
+        dotIsPlaced = false
+        
         switch operationSign {
         case "+" :
             operateWithTwoOperands{$0 + $1}
@@ -54,7 +66,37 @@ class ViewController: UIViewController {
         case "/" :
             operateWithTwoOperands{$0 / $1}
         default: break
-
+        }
+    }
+    @IBAction func clearButtonPressed(_ sender: UIButton) {
+        firstOperand = 0
+        secondOperand = 0
+        currentInput = 0
+        displayResultLabel.text = "0"
+        stillTyping = false
+        operationSign = ""
+        dotIsPlaced = false
+    }
+    @IBAction func plusMinusButtonPressed(_ sender: UIButton) {
+        currentInput = -currentInput
+    }
+    @IBAction func percentageButtonPressed(_ sender: UIButton) {
+        if firstOperand == 0 {
+            currentInput = currentInput / 100
+        }else{
+            secondOperand = firstOperand * currentInput * 100
+        }
+        stillTyping = false
+    }
+    @IBAction func squareRootButtonPressed(_ sender: UIButton) {
+        currentInput = sqrt(currentInput)
+    }
+    @IBAction func dotButtonPressed(_ sender: Any) {
+        if stillTyping && !dotIsPlaced {
+            displayResultLabel.text = displayResultLabel.text! + "."
+            dotIsPlaced = true
+        }else if !stillTyping && !dotIsPlaced {
+            displayResultLabel.text = "0."
         }
     }
 }
